3fda96f27b7637b88577388990f2f01e0178e220
Mark Wild <mark.wild189@gmail.com>
2020-06-20 18:57:49 -0700

    commit

--- src/components/main/main.component.jsx
+++ src/components/main/main.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import About from '../about/about.component';
+import Projects from '../projects/projects.component';
 
 import './main.styles.scss';
 
 const Main = () => (
   <div>
     <About />
+    <Projects />
   </div>
 );
 
