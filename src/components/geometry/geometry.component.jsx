1bea40fe4ffd3ea439ffe7286a766b16289c8889
Mark Wild <mark.wild189@gmail.com>
2020-06-22 13:06:46 -0700

    add background geometry scroll event

--- src/components/geometry/geometry.component.jsx
+++ src/components/geometry/geometry.component.jsx
@@ -4,38 +4,15 @@ import './geometry.styles.scss';
 
 class Geometry extends Component {
   componentDidMount() {
-    // var scene = new THREE.Scene();
-    // var camera = new THREE.PerspectiveCamera(
-    //   75,
-    //   window.innerWidth / window.innerHeight,
-    //   0.1,
-    //   1000
-    // );
-    // var renderer = new THREE.WebGLRenderer();
-    // renderer.setSize(window.innerWidth, window.innerHeight);
-    // // document.body.appendChild( renderer.domElement );
-    // // use ref as a mount point of the Three.js scene instead of the document.body
-    // this.mount.appendChild(renderer.domElement);
-    // var geometry = new THREE.BoxGeometry(1, 1, 1);
-    // var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    // var cube = new THREE.Mesh(geometry, material);
-    // scene.add(cube);
-    // camera.position.z = 5;
-    // var animate = function () {
-    //   requestAnimationFrame(animate);
-    //   cube.rotation.x += 0.01;
-    //   cube.rotation.y += 0.01;
-    //   renderer.render(scene, camera);
-    // };
-    // animate();
     var scene = new THREE.Scene();
+    scene.background = null;
     var camera = new THREE.PerspectiveCamera(
-      50,
+      55,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
-    var renderer = new THREE.WebGLRenderer();
+    var renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     // document.body.appendChild( renderer.domElement );
     // use ref as a mount point of the Three.js scene instead of the document.body
@@ -43,7 +20,7 @@ class Geometry extends Component {
     var geometry = new THREE.IcosahedronGeometry();
     //directional light
     {
-      const color = 0xffffff;
+      const color = '0xffffff';
       const intensity = 1;
       const light = new THREE.DirectionalLight(color, intensity);
       light.position.set(-1, 2, 4);
@@ -51,22 +28,52 @@ class Geometry extends Component {
     }
     const material = new THREE.MeshNormalMaterial({
       color: 0x44aa88,
-      wireframe: true,
+      // wireframe: true,
     });
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
     camera.position.z = 5;
+
     var animate = function () {
       requestAnimationFrame(animate);
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
+
+      render();
     };
+
+    var mouseX = 0,
+      mouseY = 0;
+
+    var windowHalfX = window.innerWidth / 2;
+    var windowHalfY = window.innerHeight / 2;
+
+    function onDocumentMouseMove(event) {
+      mouseX = (event.clientX - windowHalfX) / 175;
+      mouseY = (event.clientY - windowHalfY) / 175;
+    }
+
+    function render() {
+      camera.position.x += (mouseX - camera.position.x) * 0.5;
+      camera.position.y += (-mouseY - camera.position.y) * 0.5;
+
+      camera.lookAt(scene.position);
+
+      renderer.render(scene, camera);
+    }
+
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+
     animate();
   }
   render() {
     return (
-      <div className='geometry-background' ref={(ref) => (this.mount = ref)} />
+      <div className='geometry-container'>
+        <div
+          className='geometry-background'
+          ref={(ref) => (this.mount = ref)}
+        />
+      </div>
     );
   }
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,20 +4,20 @@ import './header.styles.scss';
 import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => (
-  <Navbar bg='light' expand='xl' className='fixed-top'>
+  <Navbar bg='white' expand='xl' className='fixed-top shadow-sm'>
     <Navbar.Brand href='#home'>
       <img src={require('../../assets/logo.png')} alt='logo' className='logo' />
     </Navbar.Brand>
     <Navbar.Toggle aria-controls='basic-navbar-nav' />
     <Navbar.Collapse id='basic-navbar-nav'>
       <Nav className='ml-auto nav '>
-        <Nav.Link className='ml-4 mr-4 h5' href='#home'>
+        <Nav.Link className='ml-4 mr-4 h6' href='#home'>
           ABOUT
         </Nav.Link>
-        <Nav.Link className='ml-4 mr-4 h5' href='#link'>
+        <Nav.Link className='ml-4 mr-4 h6' href='#link'>
           PROJECTS
         </Nav.Link>
-        <Nav.Link className='ml-4 mr-4 h5' href='#link'>
+        <Nav.Link className='ml-4 mr-4 h6' href='#link'>
           CONTACT
         </Nav.Link>
       </Nav>
