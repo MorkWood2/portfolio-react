0722ae82f459bcd9aa7eda5646ae532fff2d32bb
Mark Wild <mark.wild189@gmail.com>
2020-09-22 17:12:46 -0700

    New olem img
    update olem im and minor style fixes

--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -6,24 +6,6 @@ const Projects = () => (
     <div className='project-container' id='projects'>
       <div className='project-content-container'>
         <div className='projects'>
-          <a href='https://morkwood2.github.io/neo-art/'>
-            <img src={require('../../assets/project1.png')} alt='project' />
-          </a>
-          <h1>Art_Project_2</h1>
-          <h5>Art & Design</h5>
-
-          <p>
-            A public declaration of my two greatest loves in this world. Coding
-            and art. Inspired by the vast array of public art in Oakland and in
-            the spirit of photojournalism, this project follows my journey to
-            document and showcase some of my favorite pieces in the ever
-            changing underworld of street art in the San Francisco Bay Area.
-            Leveraging Google Maps Api & Javascript, I carefully designed this
-            page to reflect the hues and cultures of the neighborhoods around
-            me, this project is the embodiment of art and composition.
-          </p>
-        </div>
-        <div className='projects'>
           <a href='https://morkwood2.github.io/covid-tracker/'>
             <img src={require('../../assets/covid.png')} alt='project' />
           </a>
@@ -51,6 +33,24 @@ const Projects = () => (
             is built with performance, and scale in mind. Tested with Jest, with
             Google’s Firebase as a backend, it is continuously deployed to
             Heroku.
+          </p>
+        </div>
+        <div className='projects'>
+          <a href='https://morkwood2.github.io/neo-art/'>
+            <img src={require('../../assets/project1.png')} alt='project' />
+          </a>
+          <h1>Art_Project_2</h1>
+          <h5>Art & Design</h5>
+
+          <p>
+            A public declaration of my two greatest loves in this world. Coding
+            and art. Inspired by the vast array of public art in Oakland and in
+            the spirit of photojournalism, this project follows my journey to
+            document and showcase some of my favorite pieces in the ever
+            changing underworld of street art in the San Francisco Bay Area.
+            Leveraging Google Maps Api & Javascript, I carefully designed this
+            page to reflect the hues and cultures of the neighborhoods around
+            me, this project is the embodiment of art and composition.
           </p>
         </div>
       </div>
