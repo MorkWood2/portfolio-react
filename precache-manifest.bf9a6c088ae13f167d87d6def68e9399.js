3d147320aa28e712a5739419cf1a8e1d2d6becb9
Mark Wild <mark.wild189@gmail.com>
2020-09-22 18:08:38 -0700

    Updates

---
+++ precache-manifest.bf9a6c088ae13f167d87d6def68e9399.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ff98caf66b6e6f740f52a64718eb9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d70ed3456526bd4cd67c",
+    "url": "/static/css/main.4368b747.chunk.css"
+  },
+  {
+    "revision": "dac6af4d84ab48711b94",
+    "url": "/static/js/2.08c405a3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.08c405a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d70ed3456526bd4cd67c",
+    "url": "/static/js/main.2f56ff6a.chunk.js"
+  },
+  {
+    "revision": "0934e5a0629121a12638",
+    "url": "/static/js/runtime-main.c542aba9.js"
+  },
+  {
+    "revision": "b4453ec6416b66af06617633b622e59f",
+    "url": "/static/media/covid.b4453ec6.png"
+  },
+  {
+    "revision": "5bdf382375d11635711b4f8538e3cfcb",
+    "url": "/static/media/logo.5bdf3823.png"
+  },
+  {
+    "revision": "66627efe1968a075d2e4afff5159cf23",
+    "url": "/static/media/portrait.66627efe.jpg"
+  },
+  {
+    "revision": "b76451ee5e3829011e482d4b55b4bf81",
+    "url": "/static/media/project1.b76451ee.png"
+  },
+  {
+    "revision": "d0e518860cae54c74ab50cd090fa9dcb",
+    "url": "/static/media/project2.d0e51886.png"
+  },
+  {
+    "revision": "76337a998c8ac3ec566de69b9943d8bc",
+    "url": "/static/media/project3.76337a99.png"
+  },
+  {
+    "revision": "c32f46ce71dfa45ccbb426216404590a",
+    "url": "/static/media/subproject1.c32f46ce.png"
+  },
+  {
+    "revision": "7394ed9c6b038f098ed54f626ca5e3b4",
+    "url": "/static/media/subproject2.7394ed9c.png"
+  }
+]);
